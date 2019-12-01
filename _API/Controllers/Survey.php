b59052a5ef09685b2a952e125b96556b1d57b758
Ricardo <rimendoz86@gmail.com>
2019-12-01 14:56:33 -0800

    update

--- _API/Controllers/Survey.php
+++ _API/Controllers/Survey.php
@@ -22,7 +22,7 @@ class Survey extends API\APIBase{
     }
 
     function Post($req){
-        die(json_encode($req));
+        //die(json_encode($req));
         $user = $this->Sess_Auth->get();
         if(!isset($user)){
             $this->AddValidationMessage("You must be logged in to do this.");
